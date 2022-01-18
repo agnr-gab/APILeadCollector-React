903ad3b607148856174e67d1de9905176326cdd1
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-18 14:45:24 -0300

    feat: Criar Dashboard

---
+++ src/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+    
+    render() {
+        return(
+            <div>
+                <h1>DASHBOARD</h1>
+                <h2>Você está logado no sistema</h2>
+            </div>
+        );
+    }
+
+}
+export default Dashboard;
