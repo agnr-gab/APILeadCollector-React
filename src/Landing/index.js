9ad442062566f0a7b7189d6537e1495b7e152563
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-18 14:57:35 -0300

    feat: Integrar landing page

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -1,73 +1,80 @@
-import React, { Component } from "react";
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import Header from "../Componente/Header/Header";
+import react, { Component } from "react";
+import { Form, FormGroup, Label, Input, Button, Alert } from "reactstrap";
+import Header from "../../Header";
 
-export default class LandingPage extends Component {
+export default class Landing extends Component {
 
-   /* state = {
-        model: {
-            nome: "",
-            email: "",
-            observacoes: "",
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: this.props.state ? this.state.message : "",
+    };
+  }
+
+  save = (lead) => {
+    const url = "http://localhost:8080/leads";
+    const data = {
+      nome: lead.nome,
+      email: lead.email,
+      observacoes: lead.observacoes,
+    };
+
+    const requestInfo = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      })
+    };
+
+    fetch(url, requestInfo)
+
+      .then((response) => {
+        return response
+
+      })
+
+      .catch(
+        e => {
+          this.state({ message: e.message });
+        });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header title="Landing Page" />
+        <hr />
+
+        {
+          this.state.message !== '' ? (
+            <Alert color='danger' className='text-center'>{this.state.message}</Alert>
+          ) : ''
         }
 
-    }
+        <Form className="formulario">
 
-    setValues = (e, field) => {
+          <FormGroup>
+            <Label for="nome"> Nome: </Label>
+            <Input type="text" id="nome" onChange={e => this.nome = e.target.value} placeholder="Informe o seu nome" />
+          </FormGroup>
 
-        const {model} = this.state;
-        model[field] = e.target.value;
-        this.setState({model});
-    }
+          <FormGroup>
+            <Label for="email"> Email: </Label>
+            <Input type="text" id="email" onChange={e => this.email = e.target.value} placeholder="Informe o seu email" />
+          </FormGroup>
 
-    save = (lead) => {
+          <FormGroup>
+            <Label for="observations"> Observações: </Label>
+            <Input
+              type="textarea" id="observations" onChange={e => this.observacoes = e.target.value} placeholder="Digite alguma observação" />
+          </FormGroup>
 
-        const url = "http://localhost:8080/leads";
-        let data = {
-            nome: lead.nome,
-            email: lead.email,
-            observacoes: lead.observacoes
-
-        }
-
-        const requestInfo = {
-            method: "POST",
-            body: "",
-        }
-
-        fetch(url, requestInfo)
-    }
-    */
-
-    render() {
-
-        return (
-
-            <div>
-                <Header title="Cadastro de Usuário" />
-                <hr />
-                <Form>
-
-                    <FormGroup>
-                        <Label for="name"> Nome: </Label>
-                        <Input type="text" id="name" placeholder="Digite seu nome" />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label for="email"> Email: </Label>
-                        <Input type="text" id="email" placeholder="Digite seu e-mail" />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label for="observations"> Observações: </Label>
-                        <Input type="text" id="observations" placeholder="Digite alguma observação" />
-                    </FormGroup>
-
-                    <Button color="primary" block>Enviar</Button>
-
-                </Form>
-            </div>
-
-        );
-    }
+          <Button color="danger" block onClick={this.signIn}>            
+            ENVIAR
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
